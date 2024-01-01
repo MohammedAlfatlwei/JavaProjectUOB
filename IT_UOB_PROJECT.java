eaa0d9a77475b98c7986e20f74c0f305c7820cac
mohammad <mohammad.wael.s.ajam@gmail.com>
2024-01-01 20:12:28 +0300

    Added The Inputs

--- IT_UOB_PROJECT.java
+++ IT_UOB_PROJECT.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 /**
  * IT_UOB_PROJECT
  */
 public class IT_UOB_PROJECT {
-
     public static void main(String[] args) {
-        System.out.println("wellcome to our repo ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Product Name : ");
+        String productName = scanner.nextLine();
+
+        System.out.print("The Amount of " + productName + " : ");
+        int productAmount = scanner.nextInt();
+
+        System.out.print("The Cost of " + productName + " : ");
+        double productCost = scanner.nextDouble();
+
+        System.out.print("The Price of " + productName + " : ");
+        double productPrice = scanner.nextDouble();
+
+
+        // Temporary Output
+
+        System.out.println("Product Name : " + productName + "\nAmount : " + productAmount + "\nCost : " + productCost + "\nPrice : " + productPrice);
+
     }
 }

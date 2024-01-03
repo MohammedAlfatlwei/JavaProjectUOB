5a536e0a1b953a708c3992396918b4fc3bfa1f2d
mohammed alfatlawie <mohya992@gmail.com>
2024-01-03 08:45:25 +0300

    good morning guys

--- IT_UOB_PROJECT.java
+++ IT_UOB_PROJECT.java
@@ -22,15 +22,29 @@ public class IT_UOB_PROJECT {
         System.out.print("The Amount Sold of " + productName + " : ");
         int productAmountSold = scanner.nextInt();
 
+        double totalSales = productAmountSold * productPrice ; 
+        /*
+         * we are gonna discuss the rest of the program later such as 
+         * the monthly income of all the product 
+         * and the yearly income (combined the monthly income )
+         * note that he needs not only info for one product but for the rest may be 3 or 4 
+         * so i guess we need to make a loop so that user can write the rest of the product he have 
+         * and then we calaculate all of them togather
+         */
+
 
         // Temporary Output
+        System.out.println("________________________________________________________________________________________");
+
 
         System.out.println(
                 "Product Name : " + productName +
                         "\nAmount Available : " + productAmountAvailable +
                         "\nCost : " + productCost +
                         "\nPrice : " + productPrice +
-                        "\nAmount Sold : " + productAmountSold);
+                        "\nAmount Sold : " + productAmountSold +
+                        "\ntotal sales : " + totalSales
+                        );
 
 
         scanner.close();

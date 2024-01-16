03d86916bca19ff41f61c1668e4b2a1425fd38c0
mohammed alfatlawie <mohya992@gmail.com>
2024-01-16 21:43:39 +0300

    hello world

--- IT_UOB_PROJECT.java
+++ IT_UOB_PROJECT.java
@@ -1,24 +1,12 @@
 import java.util.Scanner;
+
 /**
  * IT_UOB_PROJECT
  */
 public class IT_UOB_PROJECT {
     public static void main(String[] args) {
-        /* 
-        System.out.print("how many product you sold: ");
-        Scanner productnum = new Scanner(System.in); 
-        int sales_num = productnum.nextInt();
-        */ 
-
-        /*i dont understand why the compiler rejecting me when i use (while loop) and (Do-while loop) */
-
-        /*
-        what im trying to do is to take a number form the user of how many product they sold 
-         * and then using that ualue (the number) in while loop condtion to rpeat over and over the process of calcualting 
-         */
-        /*
-         * i like that to write comment hahaha
-         */
+        
+       
             
         
         Scanner scanner = new Scanner(System.in);
@@ -41,14 +29,7 @@ public class IT_UOB_PROJECT {
         double total_product_slaes = productAmountSold * productPrice ;
         
         
-        /*
-         * we are gonna discuss the rest of the program later such as 
-         * the monthly income of all the product 
-         * and the yearly income (combined the monthly income )
-         * note that he needs not only info for one product but for the rest may be 3 or 4 
-         * so i guess we need to make a loop so that user can write the rest of the product he have 
-         * and then we calaculate all of them togather
-         */
+       
 
 
         // Temporary Output
@@ -57,16 +38,16 @@ public class IT_UOB_PROJECT {
 
 
         System.out.println(
-                "Product Name : " + productName +
+                        "Product Name : " + productName +
                         "\nAmount Available : " + productAmountAvailable +
                         "\nCost : " + productCost +
                         "\nPrice : " + productPrice +
                         "\nAmount Sold : " + productAmountSold +
                         "\ntotal sales : " + total_product_slaes + "$"
                         );
-
-
-        scanner.close();
-        
     }
 }
+
+ 
+
+
